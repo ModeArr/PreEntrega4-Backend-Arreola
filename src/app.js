6adfedf2b47210d2e1dca20703cc81969c5fadbf
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2024-01-29 23:48:36 -0600

    add handlebars views and support

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 const express = require("express")
+const handlebars = require("express-handlebars")
+const path = require("path")
 const productsRoutes = require("./routes/products.routes")
 const cartsRoutes = require("./routes/carts.routes")
+const viewRoutes = require("./routes/views.routes")
 
 PORT = 8080
 API_PREFIX = "api"
 
 const app = express()
 
+
+
 app.use(express.urlencoded({ extends: true }));
 app.use(express.json()); 
 app.use(express.static(__dirname + '/public'))
 
+app.engine("handlebars", handlebars.engine())
+app.set("views", path.join(`${__dirname}/views`))
+app.set("view engine", "handlebars")
+
 app.use(`/${API_PREFIX}/products`, productsRoutes)
 app.use(`/${API_PREFIX}/carts`, cartsRoutes)
+app.use('/', viewRoutes)
 
 app.listen(PORT, () => {
     console.log("SERVER FUNCIONANDO")
---
+++ src/routes/views.routes.js
@@ -0,0 +1,36 @@
+const { Router } = require("express")
+const path = require("path");
+const pathDB = path.join(`${__dirname}/../products.json`)
+const ProductManager = require("../ProductManager");
+const products = new ProductManager(pathDB)
+
+const router = Router()
+
+router.get('/', (req, res) => {
+
+    products.getProducts().then(result => {
+        res.render("index", {
+            title: "PreEntrega4-Backend",
+            products: JSON.stringify(result) 
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json(err.message);
+    });
+})
+
+router.get('/realtimeproducts', (req, res) => {
+
+    products.getProducts().then(result => {
+        res.render("realtimeproducts", {
+            title: "PreEntrega4-Backend - Productos en tiempo real",
+            products: JSON.stringify(result) 
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json(err.message);
+    });
+})
+
+
+module.exports = router
